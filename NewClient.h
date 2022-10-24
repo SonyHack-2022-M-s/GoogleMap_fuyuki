ac34e6908f1a37b5a58fc84a51c0556caea91d9b
fykey <fuyuki-o@globe.ocn.ne.jp>
2022-10-24 12:14:59 +0900

    make new client library

---
+++ NewClient.h
@@ -0,0 +1,23 @@
+#ifndef NewClient_h
+#define NewClient_h
+
+/*
+this client source need these function
+
+boolean lient->connect(HOST, SSL_PORT) 
+>> when connect the host return true
+
+void client->println(string);
+>> send GET command to server
+
+client->available()
+>>from sending get request to getting the response
+
+client->read();
+>>client read to char c(maybe json to char)->
+
+
+*/
+
+
+#endif
